988946bd403813a334cf604532033a3d487886dc
segakuz <segakuz@gmail.com>
2021-10-07 17:40:20 +0500

    lesson(3.5): passed

---
+++ 35_2_mark.py
@@ -0,0 +1,30 @@
+import pytest
+from selenium import webdriver
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+
+class TestMainPage1():
+
+    @pytest.mark.smoke
+    def test_guest_should_see_login_link(self, browser):
+        browser.get(link)
+        browser.find_element_by_css_selector("#login_link")
+
+    @pytest.mark.regression
+    def test_guest_should_see_basket_link_on_the_main_page(self, browser):
+        browser.get(link)
+        browser.find_element_by_css_selector(".basket-mini .btn-group > a")
+
+# pytest -sv -m smoke lessons/35_2_marks.py
+# pytest -sv -m "not smoke" lessons/35_2_marks.py
+# pytest -sv -m "smoke or regression" lessons/35_2_marks.py
